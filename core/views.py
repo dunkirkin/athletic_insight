140ca40c1c79cd4d81e2b31d438021e3407b7d8c
Jenni :3 <jnguyen1259@gmail.com>
2026-02-04 17:51:24 -0800

    testing urls; added core/urls.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def about(request):
+    return HttpResponse("This is the About page, testing stuff rn")
