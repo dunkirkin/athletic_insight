e3a1f39139affec464ad9879854bb0c3263efeb8
Jenni :3 <jnguyen1259@gmail.com>
2026-02-11 12:53:42 -0800

    removed core and added pages instead. added home page and logs, will figure out how to do entries in logs later

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from pages.views import home_view, logs_view
+
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('logs/', logs_view),
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),
 ]
